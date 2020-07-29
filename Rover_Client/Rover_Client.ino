c0395d5b71af5a42512eee36b58cd24fb144f904
NullCub3 <tigercraft3k@gmail.com>
2020-07-29 10:24:53 -0700

    Defining Variables in Arduino

--- Rover_Client/Rover_Client.ino
+++ Rover_Client/Rover_Client.ino
@@ -1,3 +1,14 @@
+#define enA 10
+#define enb 5
+#define in1 9
+#define in2 8
+#define in3 7
+#define in4 6
+
+int inputPWM
+int pwmOutput
+int
+
 void setup() {
   // put your setup code here, to run once:
 
